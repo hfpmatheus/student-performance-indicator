8abfd2af9da988a79259c86e843c58ddcf9eb837
hfpmatheus <matheushffp@gmail.com.br>
2023-11-28 08:32:33 -0300

    bug fixing

--- src/exception.py
+++ src/exception.py
@@ -17,7 +17,7 @@ def format_error_message(error_message, error_detail: sys):
 # This line defines a custom exception class CustomException that inherits from the built-in Exception class.
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = format_error_message(error=error_message, error_detail=error_detail)
 
     def __str__(self):
